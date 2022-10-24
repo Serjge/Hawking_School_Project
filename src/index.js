f47b5e3e4d34197f3fb3e819a996f02e9a8c3104
Serjge <serjge@yandex.ru>
2022-10-24 16:54:13 +0300

    feat: add burger menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles/index.scss'
+import $ from "jquery";
+
+$(document).ready(function () {
+  $('.burger').click(function () {
+    $('.burger__line,.menu__container').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
