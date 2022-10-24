2843dac9dfbd555b5710f326736e2bdc7a3d82ad
Serjge <serjge@yandex.ru>
2022-10-24 21:40:28 +0300

    feat: add submit form in console

--- src/index.js
+++ src/index.js
@@ -7,3 +7,26 @@ $(document).ready(function () {
     $('body').toggleClass('lock');
   });
 });
+
+function serializeForm(formNode) {
+  const { elements } = formNode
+  const data = Array.from(elements)
+    .filter((item) => !!item.name)
+    .map((element) => {
+      const { name, value } = element
+      return { name, value }
+    })
+  console.log(data)
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault()
+  serializeForm(applicantForm)
+}
+
+
+
+const applicantForm = document.getElementById('form')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
+
